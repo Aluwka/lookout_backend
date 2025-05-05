f438f11168dd8303cc15fff2882158f4556009a2
Kaziyeva Dina <gitEamil@example.com>
2025-05-05 17:17:02 +0500

    commit

--- src/core/app/app_creator.py
+++ src/core/app/app_creator.py
@@ -20,7 +20,10 @@ class AppCreator:
     def add_cors(self, allow_origins: List[str]) -> None:
         self._app.add_middleware(
             CORSMiddleware,
-            allow_origins=allow_origins,
+            allow_origins=[
+                "http://localhost:63342",       # ← Your frontend origin
+                "http://127.0.0.1:63342",       # ← Some browsers rewrite localhost to 127.0.0.1
+            ],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
